db9d93d571b050b3ec5fd2da99b9b055687aa537
Failedmush <anytimewicked@gmail.com>
2026-04-04 23:34:54 +0000

    tfa98xx: Fix syntax errors and remove merge conflict markers

--- techpack/audio/asoc/codecs/tfa98xx/src/tfa98xx.c
+++ techpack/audio/asoc/codecs/tfa98xx/src/tfa98xx.c
@@ -4023,18 +4023,13 @@ static int tfa98xx_i2c_probe(struct i2c_client *i2c,
 
 	/* Power up! */
     /* we should reset chip only 1 times if all reset pin connected to 1 GPIO. */
-<<<<<<< Updated upstream
     if (0 == tfa98xx_device_count) {
-=======
-    if (0 == tfa98xx_device_count) {
->>>>>>> Stashed changes
-    	tfa98xx_ext_reset(tfa98xx);
-	}
+        tfa98xx_ext_reset(tfa98xx);
+    }
 
 	if ((no_start == 0) && (no_reset == 0)) {
 		ret = regmap_read(tfa98xx->regmap, 0x03, &reg);
 		if (ret < 0) {
-	}
 			dev_err(&i2c->dev, "Failed to read Revision register: %d\n",
 				ret);
 			return -EIO;
